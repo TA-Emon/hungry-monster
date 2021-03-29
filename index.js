d1ac54f01dfa3fc94c65f54cec0a54eccfa433da
TA-Emon <tanviremon726@gmail.com>
2021-03-29 19:04:24 +0600

    modified

--- index.js
+++ index.js
@@ -1,27 +1,37 @@
-const searchButton= document.getElementById('search-button').addEventListener(click,function(){
-    const search = document.getElementById('search');
-    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${search.value}`
+
+
+const clickForSearch = () => {
+    const search = document.getElementById('search').value;
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`
     fetch(url)
     .then(res => res.json())
-    .then(data => showItems(data))
-});
-
-
-
-
-
-const showItems = food => {
-    let allMeals = document.getElementById("allMeals")
-    const meal = food.meals;
-    let meals = meal[0];
-
-    const mealInfo = `
-        <div>
-        <img src="${meals.strMealThumb}">
-        <h4>${meals.strMeal}</h4>
-        </div>
-    `
-
-    allMeals.innerHTML = mealInfo;
-    // console.log(meals);
+    .then(data => showMeals(data.meals))
+    
 }
+
+const showMeals = meal => {
+    const allMeals = document.getElementById('allMeals');
+    meal.forEach(x =>{
+        console.log(x);
+        const div = document.createElement('div');
+        div.innerHTML=`
+            <img src="${x.strMealThumb}">
+            <h2>${x.strMeal}</h2>
+        `
+        allMeals.appendChild(div);
+    })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
